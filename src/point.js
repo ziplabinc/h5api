b4a0b0aadf7096eb007cd4d1375337da543d5989
DekayKim <widoint@gmail.com>
2018-07-30 21:05:24 +0900

    [2.0.5] gdApi.Point 팝업 이미지/CSS 수정

--- src/point.js
+++ src/point.js
@@ -7,29 +7,19 @@ gdApi.Point = new function() {
         }
 
         this._descText = {
-            success: {
-                "header": [
-                    "<span class='bigsize'>축하합니다!</span>",
-                    "게임돔 이용으로",
-                    "<span class='gd-coin'></span><span class='gd-bold-blue'><span class='amount'></span>포인트</span>를",
-                    "획득했습니다."
-                ],
-                "body": [
-                    "<span class='gd-star'>★</span>100포인트를 모으면 100컬쳐캐쉬로<span class='gd-star'>★</span>",
-                    "<span class='gd-star'>★</span>바로 전환할 수 있습니다.<span class='gd-star'>★</span>"
-                ]
-            },
-            not_login: {
-                "header": [
-                    "게임돔 <span style='font-weight: 600'>로그인</span> 후",
-                    "이용하시면 <span class='gd-coin'></span><span class='gd-bold-blue'>포인트</span>를",
-                    "적립할 수 있습니다."
-                ],
-                "body": [
-                    "<span class='gd-star'>★</span>100포인트를 모으면 100컬쳐캐쉬로<span class='gd-star'>★</span>",
-                    "<span class='gd-star'>★</span>바로 전환할 수 있습니다.<span class='gd-star'>★</span>"
-                ]
-            },
+            success: [
+                "<div>축하합니다!</div>",
+                "<div><span class='amount'></span>P</div>"
+            ],
+            not_login: [
+                "<div>악!! 포인트는</div>",
+                "<div>로그인 상태에서만</div>",
+                "<div>적립됩니다.</div>"
+            ],
+            body: [
+                "<span class='highlight'>100포인트</span>를 모으면",
+                "<span class='highlight'>100컬쳐캐쉬</span>로 즉시 전환"
+            ]
         };
 
         this.DOM.backScreen = document.createElement("div");
@@ -46,32 +36,39 @@ gdApi.Point = new function() {
         this.DOM.mainPopup.id = "gd-popup";
         this.DOM.backScreen.appendChild(this.DOM.mainPopup);
 
-        // POPUP 내 붙이는 태그
-        var cornerTag = document.createElement("div");
-        cornerTag.id = "gd-corner-tag";
-            this.DOM.cornerTagSpan = document.createElement("span");
-            this.DOM.cornerTagSpan.innerText = "적립";
-            cornerTag.appendChild(this.DOM.cornerTagSpan);
-        this.DOM.mainPopup.appendChild(cornerTag);
-
+        this.DOM.topImage = document.createElement("div");
+        this.DOM.topImage.id = "gd-top-image";
+        this.DOM.mainPopup.appendChild(this.DOM.topImage);
 
         this.DOM.successText = document.createElement("div");
         this.DOM.successText.id = "gd-text-success";
-        this.DOM.successText.innerHTML = "<h2 class='gd-text-header'></h2><span class='gd-text-body'></span>";
+        this.DOM.successText.classList.add("gd-text-header");
+        this.DOM.successText.innerHTML = this._descText.success.join("");
         this.DOM.mainPopup.appendChild(this.DOM.successText);
-        this.DOM.successText.getElementsByClassName("gd-text-header")[0].innerHTML
-         = this._descText.success.header.join("<br>");
-        this.DOM.successText.getElementsByClassName("gd-text-body")[0].innerHTML
-         = this._descText.success.body.join("<br>");
         
         this.DOM.notloginText = document.createElement("div");
         this.DOM.notloginText.id = "gd-text-notlogin";
-        this.DOM.notloginText.innerHTML = "<h2 class='gd-text-header'></h2><span class='gd-text-body'></span>";
+        this.DOM.notloginText.classList.add("gd-text-header");
+        this.DOM.notloginText.innerHTML = this._descText.not_login.join("");
         this.DOM.mainPopup.appendChild(this.DOM.notloginText);
-        this.DOM.notloginText.getElementsByClassName("gd-text-header")[0].innerHTML
-         = this._descText.not_login.header.join("<br>");
-        this.DOM.notloginText.getElementsByClassName("gd-text-body")[0].innerHTML
-         = this._descText.not_login.body.join("<br>");
+        
+        var bodyText = document.createElement("div");
+        bodyText.classList.add("gd-text-body");
+        bodyText.innerHTML = this._descText.body.join("<br>");
+        this.DOM.mainPopup.appendChild(bodyText);
+        
+        var buttonBox = document.createElement("div");
+        buttonBox.id = "gd-button-box";
+        this.DOM.mainPopup.appendChild(buttonBox);
+
+        this.DOM.loginBtn = document.createElement("div");
+        this.DOM.loginBtn.id = "gd-login";
+        this.DOM.loginBtn.classList.add("gd-button");
+        this.DOM.loginBtn.innerText = "로그인 하기";
+        this.DOM.loginBtn.addEventListener("click", function(e) {
+            parent.parent.location.href="/access?mode=login";
+        }.bind(this));
+        buttonBox.appendChild(this.DOM.loginBtn);
 
         var submitBtn = document.createElement("div");
         submitBtn.classList.add("gd-button");
@@ -82,16 +79,8 @@ gdApi.Point = new function() {
             if(typeof this.resumeGame == "function")    this.resumeGame();
             if(typeof this.sucsback == "function")      this.sucsback();
         }.bind(this));
-        this.DOM.mainPopup.appendChild(submitBtn);
+        buttonBox.appendChild(submitBtn);
 
-        this.DOM.loginBtn = document.createElement("div");
-        this.DOM.loginBtn.id = "gd-login";
-        this.DOM.loginBtn.classList.add("gd-button");
-        this.DOM.loginBtn.innerText = "로그인";
-        this.DOM.loginBtn.addEventListener("click", function(e) {
-            parent.parent.location.href="/access?mode=login";
-        }.bind(this));
-        this.DOM.mainPopup.appendChild(this.DOM.loginBtn);
     }.bind(this)
 
 
@@ -105,8 +94,16 @@ gdApi.Point = new function() {
         }
         
         if(data.amount !== undefined) {
-            // 왜 no-this?
-            gdApi.Point.DOM.mainPopup.getElementsByClassName("amount")[0].innerText = data.amount;
+            var num = data.amount + "";
+            var point = num.length % 3 ;
+           
+            var str = num.substring(0, point); 
+            while (point < num.length) {
+                if (str != "") str += ","; 
+                str += num.substring(point, point + 3); 
+                point += 3; 
+            }
+            this.DOM.mainPopup.getElementsByClassName("amount")[0].innerText = str;
         }
         
         if(data.status == "not_login") {
@@ -114,18 +111,14 @@ gdApi.Point = new function() {
             this.DOM.notloginText.style.display = "block";
 
             this.DOM.loginBtn.style.display = "block";
-            this.DOM.cornerTagSpan.innerText = "안내";
-            this.DOM.mainPopup.style.backgroundPosition = "center 163px";
-            this.DOM.mainPopup.style.backgroundImage = "url("+gdApi.style.imageURI.bgNotlogin+")";
+            this.DOM.topImage.style.backgroundImage = "url("+gdApi.style.imageURI.bgNotlogin+")";
         }
         else if(data.status == "success") {
             this.DOM.notloginText.style.display = "none";
             this.DOM.successText.style.display = "block";
 
             this.DOM.loginBtn.style.display = "none";
-            this.DOM.cornerTagSpan.innerText = "적립";
-            this.DOM.mainPopup.style.backgroundPosition = "center -8px";
-            this.DOM.mainPopup.style.backgroundImage = "url("+gdApi.style.imageURI.bgSuccess+")";
+            this.DOM.topImage.style.backgroundImage = "url("+gdApi.style.imageURI.bgSuccess+")";
         }
 
         this.DOM.backScreen.style.display = "block";
